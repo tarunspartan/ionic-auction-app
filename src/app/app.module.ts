2b9882bc58209b952eac9eaf4aea675b219d6ac6
Tarun M <tarunspartan1234@outlook.com>
2019-05-14 14:07:06 +0530

    made changes to firebaseConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,12 +28,12 @@ import { IonicImageViewerModule } from "ionic-img-viewer";
 // import { PopoverPage } from "../pages/home/";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDLutLNsZk7Cshpu7RkIRMhRo_bTnlXWNA",
-  authDomain: "auctionapp-a1f6d.firebaseapp.com",
-  databaseURL: "https://auctionapp-a1f6d.firebaseio.com",
-  projectId: "auctionapp-a1f6d",
-  storageBucket: "auctionapp-a1f6d.appspot.com",
-  messagingSenderId: "651258766641"
+  apiKey: "<API KEY>",
+  authDomain: "<authDomain>",
+  databaseURL: "<databaseURL>",
+  projectId: "<projectID>",
+  storageBucket: "<storageBucket>",
+  messagingSenderId: "<messagingSenderId>"
 };
 firebase.initializeApp(firebaseConfig);
 
